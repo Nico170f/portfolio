3dd39c51caf2b77ab341c7f7ffb10206b70186d3
Nicolai <pjevert03@gmail.com>
2023-09-29 21:25:52 +0200

    Moved footer into it's own component

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  constructor(
+    private _snackBar: MatSnackBar,
+    private clipboard: Clipboard
+  ) { }
+
+  openLink(text: string) {
+    window.open(text, '_blank');
+  }
+
+  copy(text: string) {
+    this._snackBar.open('Copied to clipboard', undefined, {
+      duration: 750,
+      horizontalPosition: 'center',
+    });
+
+    this.clipboard.copy(text);
+  }
 
 }
