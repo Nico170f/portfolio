c307e698a1b78ab8e03f2ebeee8325f1997fad11
Nicolai <pjevert03@gmail.com>
2023-09-30 11:35:05 +0200

    Setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { AfkccComponent } from './components/projects/afkcc/afkcc.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: LandingComponent
+  // },
+  {
+    path: 'afkconsoleclient',
+    component: AfkccComponent
+  },
+  {
+    path: '**',
+    component: LandingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
