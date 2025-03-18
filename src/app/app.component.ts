d9450bf110e8c3317a87ae574734d078773b034c
Nicolai <pjevert03@gmail.com>
2025-03-18 20:06:42 +0100

    Add landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import type { TransferState } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(
+    private state: TransferState,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {}
 }
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -2,9 +2,10 @@ import type { ApplicationConfig } from '@angular/core';
 import { mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
+  providers: [provideServerRendering(), provideHttpClient(withFetch())],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
