18ab648da4ecf0b5530aafc8f5fad072e4343fa2
Nicolai <pjevert03@gmail.com>
2025-03-18 17:47:18 +0100

    First commit

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettier = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      //'plugin:prettier/recommended', // Ensures Prettier is integrated
+    ],
+    plugins: {
+      prettier: prettier,
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      'prettier/prettier': ['error', { 
+        singleQuote: true, 
+        printWidth: 100,
+        endOfLine: 'auto'  // This will respect the line endings of each file
+      }],
+
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+  prettierConfig,
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  },
+);
