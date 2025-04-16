8ccdfbead8e8b755d97ef8e909f1546b892995e6
Nicolai Hansen <pjevert03@gmail.com>
2025-04-17 00:49:40 +0200

    Add Valoks section with project details, styling improvements, and tooltip functionality

--- src/app/pages/landingpage/valoks-section/valoks-section.component.ts
+++ src/app/pages/landingpage/valoks-section/valoks-section.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { registerLocaleData } from '@angular/common';
+import localeDa from '@angular/common/locales/da';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+registerLocaleData(localeDa);
 
 interface Project {
   title: string;
   description: string;
   logoPath: string;
   chips: TechChip[];
+  showToolTip?: boolean;
 }
 
 type ChipType =
@@ -32,12 +39,16 @@ interface TechChip {
 
 @Component({
   selector: 'app-valoks-section',
-  imports: [MatCardModule, MatChipsModule],
+  imports: [MatCardModule, MatChipsModule, MatIconModule, MatTooltipModule],
   templateUrl: './valoks-section.component.html',
   styleUrl: './valoks-section.component.scss',
 })
 export class ValoksSectionComponent {
   imagesPath = '../../../../assets/images/';
+  valoksWebsiteURL = 'https://valoks.com';
+  valoksGithubURL = 'https://github.com/valoks';
+  valoksLinkedInURL = 'https://www.linkedin.com/company/valoks/';
+  mccYoutubeURL = 'https://www.youtube.com/@mccvaloks';
 
   chips: Record<ChipType, TechChip> = {
     ts: {
@@ -112,13 +123,12 @@ export class ValoksSectionComponent {
         this.chips['csharp'],
         this.chips['mineflayer'],
       ],
+      showToolTip: true,
     },
     {
       title: 'Web Console Client',
-      description: `The AFK Console Client is a free, widely-used tool with over 5.000 users and
-                90.000+ launches. Seamlessly connect tons
-                of accounts while using significantly less resources.`,
-      logoPath: `acc-free1.svg`,
+      description: `This is sample text for the Web Console Client. It is a free, widely-used tool with over 5.000 users and`,
+      logoPath: `temp.svg`,
       chips: [
         this.chips['ts'],
         this.chips['express'],
@@ -128,6 +138,46 @@ export class ValoksSectionComponent {
         this.chips['docker'],
         this.chips['redis'],
       ],
+      showToolTip: true,
+    },
+    {
+      title: 'Coming soon...?',
+      description: `Big things are coming soon!`,
+      logoPath: `loading.svg`,
+      chips: [],
+    },
+  ];
+
+  stats = [
+    {
+      value: '13.440.600+',
+      description: 'Minecraft Bots Connected',
+      icon: 'android',
+      toolTipText: 'Total number of Minecraft bots connected from our app',
+    },
+    {
+      value: '27.105+',
+      description: 'Unique Users',
+      icon: 'person',
+      toolTipText: `Total number of unique users who've verified using Discord`,
+    },
+    {
+      value: '93.956+',
+      description: 'App Launches',
+      icon: 'rocket_launch',
+      toolTipText: 'Total number of times our app has been launched',
+    },
+    {
+      value: '13.068+',
+      description: 'Unique Servers',
+      icon: 'sports_esports',
+      toolTipText: 'Total number of unique Minecraft servers connected to by our app',
+    },
+    {
+      value: '20.540+',
+      description: 'Discord Members',
+      icon: 'groups',
+      toolTipText: 'Total number of Discord members in our server',
     },
   ];
 }
